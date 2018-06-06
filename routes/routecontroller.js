77186e0064cd25b7a556b0af53268af7c0a4495a
Glitch (fcc-iissuetracker) <none>
2018-06-06 05:42:43 +0000

    Ready for release

--- routes/routecontroller.js
+++ routes/routecontroller.js
@@ -2,6 +2,7 @@ var url = process.env.DB
 var dbName = 'fcc-issuetracker'
 var collName = 'issues'
 var mongo=require('mongodb').MongoClient
+var {ObjectId} = require('mongodb')
 
 var populateNewRec=function(req) {
   var newRec = {} 
@@ -16,35 +17,32 @@ var populateNewRec=function(req) {
   newRec.updated_on=(new Date()).toString()
   newRec.open=true
   return newRec
-}
+} 
 
 exports.delete = function(req,res){
   
-  if (req.body._id=='') {
+  if (req.body._id=='' || req.body._id==undefined) {
     res.send('_id error')
   } else {
     
     mongo.connect(url,function(err,db) {
-    console.log(err)
     if (err) {
       res.send(JSON.stringify(err))
     } else {
-
       //console.log(db)
       var dbo=db.db(dbName)
       var coll = dbo.collection(collName)
-      var result=coll.remove({_id:req.body._id},function(err,data){
-        console.log(data['result']['n'])
-      
+      coll.remove({_id:ObjectId(req.body._id)},function(err,data){
         if (err ) {
           console.log('Error:'+err)
-          res.send('could not update '+req.body._id)
-        } else if (data['result']['n']==0) {
+          res.send('error updating '+req.body._id)
+        } else if (data.result.n==0) {
+          console.log(data)
           res.send('could not update '+req.body._id)
         } else {
           res.send('successfully updated')
         }
-        db.close()    
+      db.close()    
     })
     }   
   })
@@ -55,10 +53,15 @@ exports.delete = function(req,res){
 exports.post = function(req,res){
   
   var newRec = populateNewRec(req)
-  
+  //console.log(newRec)
+  if (newRec.issue_title == undefined ||
+      newRec.issue_text == undefined ||
+      newRec.created_by == undefined) {
+    res.send('required fields missing')
+  } else {
   mongo.connect(url,function(err,db) {
-  console.log(err)
   if (err) {
+  console.log(err)
     res.send(JSON.stringify(err))
   } else {
     
@@ -69,58 +72,53 @@ exports.post = function(req,res){
     coll.insert(newRec,function(err,data) {
     if (err) {
       console.log('Error:'+err)
-    } else {
-      console.log('NewRec:'+JSON.stringify(newRec))
-    }
+    } 
     
     db.close()
     res.send(newRec)
     })
   }
-  
-  
-})
+  })
+  }
 }
 
 exports.put = function(req,res){
   
-  console.log(req.body)
-  if ((req.body.issue_title=='') &&
-    (req.body.issue_text=='') &&
-    (req.body.created_by=='') &&
-    (req.body.assigned_to=='') &&
-    (req.body.open==null || req.body.open==false) &&
-    (req.body.status_text=='')) {
+  //console.log(req.body)
+  if ((req.body.issue_title=='' || req.body.issue_title==undefined) &&
+    (req.body.issue_text=='' || req.body.issue_text==undefined) &&
+    (req.body.created_by=='' || req.body.created_by==undefined) &&
+    (req.body.assigned_to=='' || req.body.assogmed_to==undefined) &&
+    (req.body.open==null || req.body.open==false || req.body.open==undefined) &&
+    (req.body.status_text=='' || req.body.status_text==undefined)) {
     res.send('no updated field sent')
   } else { 
-    
-
   var newRec = populateNewRec(req)
-
+  //console.log(newRec)
   mongo.connect(url,function(err,db) {
-  console.log(err)
   if (err) {
+    console.log(err)
     res.send(JSON.stringify(err))
   } else {
-
     //console.log(db)
     var dbo=db.db(dbName)
     var coll = dbo.collection(collName)
     newRec.updated_on=(new Date()).toString()
+    
     newRec.open=req.body.open
 
     coll.update({_id:req.body._id},newRec,function(err,count) {
-      var cnt=JSON.parse(count)
-      console.log(cnt.n)
       
+      //console.log(count)
+
       if (err ) {
         console.log('Error:'+err)
         res.send('could not update '+req.body._id)
-      } else if (cnt.n==0) {
-        res.send('could not update '+req.body._id)
+      } else if (count.n==0) {
+        res.send('id not found '+req.body._id)
       
       } else {
-        console.log('Rec:'+JSON.stringify(newRec))
+        //console.log('Rec:'+JSON.stringify(newRec))
         res.send('successfully updated')
       }
       db.close()    
@@ -131,29 +129,35 @@ exports.put = function(req,res){
 }
     
 
-    
-
-
 exports.get = function(req,res){
+  // collect params for search
+  var params = req.params
+  if (req.body.issue_title!==undefined){params.issue_title=req.body.issue_title}
+  if (req.body.issue_text!==undefined){params.issue_text = req.body.issue_text}
+  if (req.body.created_by!==undefined){params.create_by = req.body.created_by}
+  if (req.body.assigned_to!==undefined){params.assigned_to = req.body.assigned_to}
+  if (req.body.status_text!==undefined){params.status_text = req.body.status_text}
+  if (req.body.created_on!==undefined){params.create_on=req.body.created_on}
+  if (req.body.updated_on!==undefined){params.updated_on=req.body.updated_on}
+  if (req.body.open!==undefined){params.open=req.body.open}
+  //console.log(req)
+  //console.log(params)
   
-  mongo.connect(url,{useNewUrlParser:true},function(err,db) {
-  if (err) {res.send(JSON.stringify(err))} 
+  mongo.connect(url,function(err,db) {
+  if (err) {res.send(JSON.stringify(err))
+  } else {
 
-  var dbo=db.db(dbName)
-  var coll = dbo.collection(collName)
-  /*  coll.find({urlNum:urlNum}).toArray(function(err,docs){
-          if (err) {response.send(JSON.stringify(err))}
-      console.log(docs)
-          var result=""
-          if (docs.length>0) {
-            result=docs[0].url
-            console.log(result)    
-          }
-          db.close()
-         })
-  */
-  })
-  
+    var dbo=db.db(dbName)
+    var coll = dbo.collection(collName)
+    coll.find(params).toArray(function(err,docs){
+      if (err) {res.send(JSON.stringify(err))
+      }  else {
+        res.send(docs)
+      }
+      db.close()
+    })
+  }
+})
 }
   
 
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -29,21 +29,45 @@ suite('Functional Tests', function() {
         })
         .end(function(err, res){
           assert.equal(res.status, 200)
-          console.log(res)
-          // check all fields not null
-          //assert.isNotNull()
-          
-          
-          done();
+          assert.isNotNull(res.body.issue_title)
+          assert.isNotNull(res.body.issue_text)
+          assert.isNotNull(res.body.created_by)
+          assert.isNotNull(res.body.assigned_to)
+          assert.isNotNull(res.body.status_text)
+          done()
         });
       });
       
       test('Required fields filled in', function(done) {
-        
+       chai.request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'Title',
+          issue_text: 'text', 
+          created_by: 'Functional Test - Every field filled in',
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200)
+          assert.isNotNull(res.body.issue_title)
+          assert.isNotNull(res.body.issue_text)
+          assert.isNotNull(res.body.created_by)
+          // check all fields not null
+          done()
+        });
       });
       
       test('Missing required fields', function(done) {
-        
+       chai.request(server) 
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'Title', 
+          created_by: 'Functional Test - Missing required fields',
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200)
+          assert.equal(res.text,'required fields missing')
+          done()
+        });
       });
       
     });
@@ -51,15 +75,45 @@ suite('Functional Tests', function() {
     suite('PUT /api/issues/{project} => text', function() {
       
       test('No body', function(done) {
-        
+       chai.request(server) 
+        .put('/api/issues/test')
+        .send({ 
+         _id:'5b16e1d0f3f2bc11d1edaf34'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200)
+          assert.equal(res.text,'no updated field sent')
+          done()
+        });
       });
-      
-      test('One field to update', function(done) {
         
+      test('One field to update', function(done) {
+       chai.request(server)  
+        .put('/api/issues/test')
+        .send({
+           _id:'5b16e1d0f3f2bc11d1edaf34',
+          issue_title: 'Title Test One'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200)
+          assert.equal(res.text,'successfully updated')
+          done()
+        });
       });
       
       test('Multiple fields to update', function(done) {
-        
+       chai.request(server) 
+        .put('/api/issues/test')
+        .send({
+          _id:'5b16e1d0f3f2bc11d1edaf34',
+          issue_title: 'Title Test', 
+          created_by: 'Functional Test - Multiple fields to update'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200)
+          assert.equal(res.text,'successfully updated')
+          done() 
+        });
       });
       
     });
@@ -87,11 +141,43 @@ suite('Functional Tests', function() {
       });
       
       test('One filter', function(done) {
-        
+        chai.request(server)
+        .get('/api/issues/test')
+        .query({})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.isArray(res.body);
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], 'created_by');
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'open');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], '_id');
+          done();
+        });
       });
       
       test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {
-        
+        chai.request(server)
+        .get('/api/issues/test')
+        .query({})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.isArray(res.body);
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], 'created_by');
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'open');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], '_id');
+          done();
+        });
       });
       
     });
@@ -99,12 +185,27 @@ suite('Functional Tests', function() {
     suite('DELETE /api/issues/{project} => text', function() {
       
       test('No _id', function(done) {
-        
+        chai.request(server)
+        .delete('/api/issues/test')
+        .send({})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text,'_id error')
+          done();
+        });
       });
       
       test('Valid _id', function(done) {
-        
-      });
+         chai.request(server)
+        .delete('/api/issues/test')
+        //'5b16e1e90ac3431227731ad9'
+        .send({_id:'5b16e1e90ac3431227731ad9'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text,'successfully updated') 
+         done();
+        });
+     });
       
     });
 
